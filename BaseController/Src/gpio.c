3aed85bde953df28deae1f4241749af95ba0cd76
Androsov Alexander <eelleekk@gmail.com>
2020-07-21 01:25:32 +0300

    Set C13 at startup
    Signed-off-by: Androsov Alexander <eelleekk@gmail.com>

--- BaseController/Src/gpio.c
+++ BaseController/Src/gpio.c
@@ -18,4 +18,12 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
+
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
